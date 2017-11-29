dce04c9a1fe90e27f12b6e584a693f473627c72b
murtcay <murtcay@gmail.com>
2017-11-29 21:58:01 +0300

    stringe cevirildi

--- go_ck.go
+++ go_ck.go
@@ -3,31 +3,51 @@ package main
 import ( "fmt"
 "net/http"
 "io/ioutil"
-//"encoding/xml"
+"encoding/json"
+//"reflect"
+//"strings"
+//"strconv"
 )
 
-type SitemapIndex struct {
-  Locations []Locations `xml:"sitemap"`
+type result struct {
+   Currency []Currency `json:"result"`
 }
 
-type Locations struct {
-    Loc string `xml:"loc"`
+type Currency struct {
+    Cur string `json:"Currency"`
 }
 
-func (l Locations) String() string {
-  return fmt.Sprintf(l.Loc)
+func (l Currency) String() string {
+  return fmt.Sprintf(l.Cur)
 
 }
 
+//https://bittrex.com/api/v1.1/public/getmarkethistory?market=BTC-DOGE
 func main(){
   resp, _ := http.Get("https://bittrex.com/api/v1.1/public/getcurrencies")
-  bytes, _ := ioutil.ReadAll(resp.Body)
-  string_body := string(bytes)
-  fmt.Println(string_body)
+  body, _ := ioutil.ReadAll(resp.Body)
+//  string_body := string(bytes)
+//  fmt.Println(string_body)
   resp.Body.Close()
 
-  //var s SitemapIndex
-  //xml.Unmarshal(bytes, &s)
-  //fmt.Println(s.Locations)
+  s := result{}
+  json.Unmarshal(body, &s)
+fmt.Println(len(s.Currency))
+  for i:=0 ; i< len(s.Currency); i++{
+    //fmt.Println(s.Currency[i])
+
+strng := "https://bittrex.com/api/v1.1/public/getmarketsummary?market=BTC-"
+st:= s.Currency[i].Cur
+url_c:= strng+st
+
+      murat, _ := http.Get(url_c)
+      bytes, _ := ioutil.ReadAll(murat.Body)
+      string_body := string(bytes)
+      fmt.Println(string_body)
+    resp.Body.Close()
+
+      }
+//  fmt.Println(s.Currency[i])
+
 
 }
