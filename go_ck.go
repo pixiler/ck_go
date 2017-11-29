88d149d8abcfabd294de199749815b1e2df399fe
Eren <erengecer_96@hotmail.com>
2017-11-29 21:11:11 +0300

    string

--- go_ck.go
+++ go_ck.go
@@ -4,19 +4,28 @@ import ( "fmt"
 "net/http"
 "io/ioutil"
 "encoding/json"
+"strings"
 )
 
 type result struct {
    Currency []Currency `json:"result"`
+   Quantity []Quantity `json:"Quantity"`
 }
 
 type Currency struct {
     Cur string `json:"Currency"`
 }
 
+type Quantity struct {
+    Qua string `json:"Quantity"`
+}
+
 func (l Currency) String() string {
   return fmt.Sprintf(l.Cur)
 
+}
+func (k Quantity) String() string {
+  return fmt.Sprintf(k.Qua)
 }
 
 func main(){
@@ -27,7 +36,20 @@ func main(){
   resp.Body.Close()
 
   s := result{}
+  bit := result{}
   json.Unmarshal(body, &s)
-  fmt.Println(s.Currency)
+
+  for index := 0; index < len(s.Currency); index++ {
+    var url_cur string  = "https://bittrex.com/api/v1.1/public/getmarkethistory?market=BTC-"
+    //url_cur += s.Currency[index]
+    //url_cur = strings.Contains(url_cur,s.Currency[index])
+    string_body := string(s.Currency)
+    url, _ := http.Get(strings.Contains(url_cur,string_body))
+    body, _ := ioutil.ReadAll(url.Body)
+    json.Unmarshal(body, &bit)
+    fmt.Println(bit.Quantity)
+  }
+
+  //fmt.Println(s.Currency)
 
 }
